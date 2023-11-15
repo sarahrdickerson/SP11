7dd372318fb5195fd7408b2d7e2d601bd21e30d5
sarahrdickerson <sarahrdickerson@gmail.com>
2023-11-15 17:48:46 -0600

    test

--- webapp/app/page.js
+++ webapp/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
     <>
       <div className=" h-full flex-col md:flex">
         <div className="container flex flex-col items-start justify-between space-y-2 py-4 sm:flex-row sm:items-center sm:space-y-0 md:h-16">
-          <h2 className="text-lg font-semibold">SP11 Music Generator</h2>
+          <h2 className="text-lg font-semibold">SP11 Music Generator App</h2>
         </div>
         <Separator />
 
