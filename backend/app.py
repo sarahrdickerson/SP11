692ce46ecee2f8e010043394095e32c01fb106d6
richard <richardphamdq@utexas.edu>
2024-01-29 21:55:26 +0000

    initial music tests

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 # make sure you are in the backend folder in cmd
-# create .venv or enter existing one (to enter existing one, skip to activate step)
-# python -m venv .venv
+# create .venv or enter existing one (to enter existing one, skip to activate step  io]\ u
 # install all packages
 # pip install -r requirements.txt
 # activate
@@ -13,6 +12,8 @@ from flask_pymongo import PyMongo
 import os
 # from dotenv import load_dotenv
 from flask_cors import CORS
+from transformers import pipeline
+import scipy
 
 # load_dotenv('.env.local')
 # mongo_password = os.getenv('MONGO_PASSWORD')
@@ -29,6 +30,13 @@ def hello_world():
 
 @app.route('/api/generate', methods=['GET', 'POST'])
 def generate():
+    return jsonify({"message": "Generating music"})
+
+@app.route('/api/generate/audiogen', methods=['GET', 'POST'])
+def generate():
+    synthesiser = pipeline("text-to-audio", "facebook/musicgen-small")
+    music = synthesiser("lo-fi music with a soothing melody", forward_params={"do_sample": True})
+    scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])
     return jsonify({"message": "Generating music"})
 
 if __name__ == '__main__':
