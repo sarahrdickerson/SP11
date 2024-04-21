bcb072250f3de9b56d12f66754f532f9df94657a
sarahrdickerson <sarahrdickerson@gmail.com>
2024-04-21 13:47:01 -0500

    increase timeout in axios config

--- api/axiosConfig.js
+++ api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5000/", // local development
   baseURL: "http://34.171.192.59:3000/", // production deployment
-  timeout: 1000, // Request timeout in milliseconds
+  timeout: 240000, // Request timeout in milliseconds
   headers: {
     "Content-Type": "application/json",
     // Add other default headers if necessary
